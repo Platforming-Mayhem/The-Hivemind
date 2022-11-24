0f1e5fc484b68b489d23b1c33298bb75f5fdb51c
Vernixs <enecelis2021@elam.co.uk>
2022-11-24 22:38:43 +0000

    Working on detection of the AI

--- Assets/Script/AI.cs
+++ Assets/Script/AI.cs
@@ -7,32 +7,59 @@ public class AI : MonoBehaviour
     public Transform target; 
     public float within_range;
     public float speed;
-    public int numberOfRays = 17;
-    public float angle = 90;
-    public float targetVelocity = 10.0f;
+    /*public GameObject player;
+    private new Camera camera;
+    private new Renderer renderer;
+    */
+    private void Start()
+    {
+       /* player = GameObject.FindGameObjectWithTag("Player");
+        camera = Camera.main;
+        renderer = GetComponent<Renderer>();*/
+    }
+    private void OnBecameVisible()
+    {
+      
+    }
     public void Update()
     {
-        
-        float dist = Vector3.Distance(target.position, transform.position);
+       /* bool isVisible = GeometryUtility.TestPlanesAABB(
+        GeometryUtility.CalculateFrustumPlanes(camera),
+        renderer.bounds);
+
+        if (!isVisible)
+        {
+            TryMovingTowardsPlayer();
+        }
+        */
+       float dist = Vector3.Distance(target.position, transform.position);
      
         if (dist <= within_range)
         {
             
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-        }
-
-
+            //transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+       }
       
     }
 
-    private void OnDrawGizmos()
+    /*private void TryMovingTowardsPlayer()
     {
-        for (int i = 0; i < numberOfRays; ++i)
-        {
-            var rotation = this.transform.rotation;
-             
-        }
+        if (player == null)
+            return;
+
+        transform.position = player.transform.position - player.transform.forward;
+        Vector3 lookPos = player.transform.position - transform.position;
+        lookPos.y = 0;
+        transform.rotation = Quaternion.LookRotation(lookPos);
     }
+    */
+
+
+
+
+
+
+
 
 
 
